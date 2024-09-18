073623f521820e5ba69276d36b3bdb8b6c9a7fb5
Jhon Sebastian Afanador Rueda <jhon.afanador@uninorte.edu.co>
2024-09-18 13:28:49 -0500

    First commit

---
+++ src/megaferia/Megaferia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package megaferia;
+
+/**
+ *
+ * @author jsafanador
+ */
+public class Megaferia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
